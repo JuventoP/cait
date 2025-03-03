7dd548ce2d347bbf26ba3e6b806cb491210e54ab
Praveen Murali <119950527+JuventoP@users.noreply.github.com>
2025-03-03 16:54:08 +0100

    modifed_nps_to_include_cps_calculation
    the original nps function i s modified to calculate the cps function between different pairs of channels and is saved to the datahandler

--- cait/versatile/analysisobjects/nps.py
+++ cait/versatile/analysisobjects/nps.py
@@ -2,18 +2,19 @@ from typing import Union
 import os
 
 import numpy as np
+from tqdm.auto import tqdm
 
 from .arraywithbenefits import ArrayWithBenefits
+from .helper import is_array_like
 from ..iterators.iteratorbase import IteratorBaseClass
 from ..eventfunctions.processing.removebaseline import RemoveBaseline
-from ..functions.apply import apply
 from ..plot.basic.line import Line
 
 from ...data import write_xy_file
 
 class NPS(ArrayWithBenefits):
     """
-    Object representing a Noise Power Spectrum (NPS). It can either be created by averaging the Fourier transformed events from an `EventIterator`, from an `np.ndarray` or read from a DataHandler or xy-file.
+    Object representing a Noise Power Spectrum (NPS) and Cross Power Spectrum (CPS). It can either be created by averaging the Fourier transformed events from an `EventIterator`, from an `np.ndarray` or read from a DataHandler or xy-file.
 
     If created from an `EventIterator`, the (constant) baseline is removed automatically.
     To improve the quality of the NPS, a window function is often applied to the noise traces before performing the Fourier transform and averaging (see Numerical Recipes by Press, Teukolsky, Vetterling, Flannery chapter 13.4.1). This can only be achieved when we still have the original noise traces, i.e. when we construct the NPS from an iterator. Instead of just a bare iterator ``it`` you can pass the iterator ``it.with_processing([vai.RemoveBaseline(), vai.TukeyFiltering()])`` to ``NPS``. 
@@ -26,15 +27,42 @@ class NPS(ArrayWithBenefits):
             self._nps = np.empty(0)
             self._n_ch = 0
         elif isinstance(data, IteratorBaseClass):
-            data = data.with_processing(RemoveBaseline())
-            self._nps = np.mean(apply(lambda x: np.abs(np.fft.rfft(x))**2, data), axis=0)
+            data = data.flatten().with_processing([RemoveBaseline(), 
+                                                lambda x: np.abs(np.fft.rfft(x))])
+            cor_id = [(0, 1), (1, 2), (2, 0)]
+            if len(data) > 1000:
+                self._nps = np.zeros_like(data.grab(0))
+                self._cps = np.zeros_like(data.grab(0))
+                self._cor = np.zeros_like(data.grab(0))
+                with data:
+                    for ev in tqdm(data, delay=5):
+                        self._nps+=ev**2
+                        if self._nps.ndim > 1:
+                            for i in range(self._nps.shape[0]):
+                                self._cps[i]+=ev[cor_id[i][0]]*ev[cor_id[i][1]]
+                self._nps/=len(data)
+                self._cps/=len(data)
+                if self._nps.ndim > 1:
+                    for i in range(self._nps.shape[0]):
+                        self._cor[i] = self._cps[i]**2/(self._nps[cor_id[i][0]]*self._nps[cor_id[i][1]])
+            else:
+                with data:
+                    self._nps = np.mean(data**2, axis=0)
+                    for ev in tqdm(data, delay=5):
+                        if self._nps.ndim > 1:
+                            for i in range(self.n_ch):
+                                self._cps[i]+=ev[cor_id[i][0]]*ev[cor_id[i][1]]
+                self._cps/=len(data)
+                if self._nps.ndim > 1:
+                    for i in range(self._n_ch):
+                        self._cor[i] = self._cps[i]**2/(self._nps[cor_id[i][0]]*self._nps[cor_id[i][1]])
             if self._nps.ndim > 1:
                 self._n_ch = self._nps.shape[0]
                 if self._n_ch == 1: self._nps = self._nps.flatten()
             else:
                 self._n_ch = 1
-        elif isinstance(data, np.ndarray):
-            self._nps = data
+        elif isinstance(data, np.ndarray) or is_array_like(data):
+            self._nps = np.array(data)
             if self._nps.ndim > 1:
                 self._n_ch = self._nps.shape[0]
                 if self._n_ch == 1: self._nps = self._nps.flatten()
@@ -44,7 +72,7 @@ class NPS(ArrayWithBenefits):
             raise ValueError(f"Unsupported datatype '{type(data)}' for input argument 'data'.")
     
     @classmethod
-    def from_dh(cls, dh, group: str = "noise", dataset: str = "nps"):
+    def from_dh(cls, dh, group: str = "noise", dataset: list = "nps"):
         """
         Construct NPS from DataHandler. 
 
@@ -60,9 +88,9 @@ class NPS(ArrayWithBenefits):
         """
         return cls(dh.get(group, dataset))
         
-    def to_dh(self, dh, group: str = "noise", dataset: str = "nps", **kwargs):
+    def to_dh(self, dh, group: str = "noise", dataset: list = ["nps", "cps"], **kwargs):
         """
-        Save NPS to DataHandler. 
+        Save NPS and CSD to DataHandler. 
 
         :param dh: The DataHandler instance to write to.
         :type dh: DataHandler
@@ -73,8 +101,11 @@ class NPS(ArrayWithBenefits):
         :param kwargs: Keyword arguments for `DataHandler.set`.
         :type kwargs: Any
         """
-        data = self._nps[None,:] if self._n_channels == 1 else self._nps
-        dh.set(group, **{dataset: data}, **kwargs)
+        data_nps = self._nps[None,:] if self._n_channels == 1 else self._nps
+        data_cps = self._cps if self._n_channels > 1 else None
+        dh.set(group, **{dataset[0]: data_nps}, **kwargs)
+        if data_cps is not None:
+            dh.set(group, **{dataset[0]: data_cps}, **kwargs)
         
     @classmethod
     def from_file(cls, fname: str, src_dir: str = ''):
